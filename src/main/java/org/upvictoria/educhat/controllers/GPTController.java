a020233f2a3d175640ff62252189d79f118deba6
RichardKobay <ricardo.roel110@gmail.com>
2024-04-15 21:11:23 -0600

    fixed requests links

--- src/main/java/org/upvictoria/educhat/controllers/GPTController.java
+++ src/main/java/org/upvictoria/educhat/controllers/GPTController.java
@@ -10,45 +10,24 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("http://localhost:5173/")
 public class GPTController {
-    @RequestMapping(value = "/get-test")
-    public @ResponseBody ChatGPTResponse test() {
-        String prompt = "Generar un examen de 30 preguntas en formato JSON sobre programación orientada a objetos (exclusivamente en java). Puedes utilizar el siguiente formato: [{'pregunta_1': 'pregunta','respuestas':['respuesta 1','respuesta 2','respuesta 3']}]. Damelos sin saltos de linea ni formato, el puro json en una sola linea. OJO, básate en los siguientes temas: Clases, Objetos, Abstracción, Encapsulamiento y polimorfismo, Herencia, Herencia Jerarquica, Casting, Tipos de Polimorfismo, Clases anidadas, Genéricos, Interfaces, Interfaces Anidadas";
 
-        prompt = "{\"model\": \"" + "gpt-3.5-turno" + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
-
+    @PostMapping("/request")
+    public ChatGPTResponse request(@RequestBody Map<String, String> requestBody) {
+        String prompt = requestBody.get("prompt");
         return new ChatGPTResponse(chatGPT(prompt));
     }
-
-    @PostMapping(value = "/chat")
-    public ChatGPTResponse chat(@RequestParam("prompt") String prompt) {
-        return new ChatGPTResponse(chatGPT(prompt));
-    }
-
 
     public String chatGPT(String body) {
         String url = "https://api.openai.com/v1/chat/completions";
         String apiKey = System.getenv("OPENAI_API_KEY");
-        String model = "gpt-3.5-turbo-0125";
 
         try {
-            URL obj = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-            connection.setDoOutput(true);
-
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-            writer.write(body);
-            writer.flush();
-            writer.close();
-
-            // Response
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader br = getBufferedReader(body, url, apiKey);
             String line;
 
             StringBuilder response = new StringBuilder();
@@ -65,6 +44,23 @@ public class GPTController {
         }
     }
 
+    private static BufferedReader getBufferedReader(String body, String url, String apiKey) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+        connection.setDoOutput(true);
+
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(body);
+        writer.flush();
+        writer.close();
+
+        // Response
+        return new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    }
+
     public String extractMessageFromJSONResponse (String response) {
         JsonObject jsonObject = new Gson().fromJson(response, JsonObject.class);
 
@@ -77,7 +73,7 @@ public class GPTController {
     }
 
 
-    static class ChatGPTResponse {
+    private class ChatGPTResponse {
         private String response;
 
         public ChatGPTResponse(String response) {
